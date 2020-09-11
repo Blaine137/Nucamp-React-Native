34e22b60c6319a73083d2b117559e575bdf75a0a
blaine young <blaine.young3553@gmail.com>
2020-09-11 10:40:07 -0400

    added content to the about component

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, Text, FlatList } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
+import { PARTNERS } from '../shared/partners';
+
+const RenderMission = (props) => {
+
+    return(
+        <Card title="Our Mission">
+            <Text>
+                We present a curated database of the best campsites 
+                in the vast woods and backcountry of the World Wide Web Wilderness. 
+                We increase access to adventure for the public while promoting safe and respectful use of resources. 
+                The expert wilderness trekkers on our staff personally verify each campsite to make sure that they 
+                are up to our standards. We also present a platform for campers to share reviews on campsites 
+                they have visited with each other.
+            </Text>
+        </Card>
+    );
+
+};
 
 class About extends Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+            partners: PARTNERS
+        };
+    }
 
     static navigationOptions = {
         title: 'About Us'
@@ -10,9 +35,25 @@ class About extends Component{
 
     render(){
 
+        const renderPartners = ({item}) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                />
+            );
+        };
+
         return(
             <ScrollView>
-
+                <RenderMission/>
+                <Card title="Community Partners">
+                    <FlatList
+                    data={this.state.partners}
+                    renderItem={renderPartners}
+                    keyExtractor={item => item.id.toString()}/>
+                </Card>
             </ScrollView>
         );
 
