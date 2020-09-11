5d66e8059ad9894dd2bf22bdae9c7daf763c50b7
blaine young <blaine.young3553@gmail.com>
2020-09-11 10:11:11 -0400

    added contact information to the contact component

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
 
 
 class Contact extends Component{
@@ -12,7 +13,23 @@ class Contact extends Component{
 
         return(
             <ScrollView>
-
+                <Card wrapperStyle={ { margin: 10 } } title="Contact Information">
+                    <Text>
+                        1 Nucamp Way
+                    </Text>
+                    <Text>
+                        Seattle, WA 98001
+                    </Text>
+                    <Text style={{marginBottom: 10}}>
+                        U.S.A.
+                    </Text>
+                    <Text>
+                        Phone: 1-206-555-1234
+                    </Text>
+                    <Text>
+                        Email: campsites@nucamp.co
+                    </Text>
+                </Card>
             </ScrollView>
         );
 
